e7d235a5298d9b30a8e84e1d3533957b3d347536
sarath menon <sarathmenon.downey@gmail.com>
2019-06-20 22:04:08 +0530

    normalize+scaled

--- gym_mobilerobot/envs/gym_mobilerobot.py
+++ gym_mobilerobot/envs/gym_mobilerobot.py
@@ -93,7 +93,7 @@ class MobileRobotGymEnv(gym.Env):
 
         self.heading = round(heading, 3)
 
-    def getState(self, scan, type='normalize'):
+    def getState(self, scan, type='normalized'):
         scan_range = []
         heading = self.heading
         min_range = 0.16
@@ -102,12 +102,12 @@ class MobileRobotGymEnv(gym.Env):
         for i in range(len(scan.ranges)):
             if scan.ranges[i] == float('Inf'):
                 if type=='normal':scan_range.append(3.5)
-                if type=='normalize':scan_range.append(1) # 3.5 / 3.5
+                if type=='normalized':scan_range.append(1) # 3.5 / 3.5
             elif np.isnan(scan.ranges[i]):
                 scan_range.append(0)
             else:
                 if type=='normal':scan_range.append(scan.ranges[i])
-                if type=='normalize':scan_range.append(scan.ranges[i] / 3.5)
+                if type=='normalized':scan_range.append(scan.ranges[i] / 3.5)
 
         if min_range > min(scan_range) > 0:
             done = True
@@ -121,7 +121,7 @@ class MobileRobotGymEnv(gym.Env):
 
         return scan_range + [heading, current_distance], done
 
-    def setReward(self, state, done,type='unscaled'):
+    def setReward(self, state, done,type='scaled'):
         current_distance = state[-1]
         heading = state[-2]
         distance_rate = (self.past_distance - current_distance)
