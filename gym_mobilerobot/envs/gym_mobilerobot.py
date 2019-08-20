17d539b855628d2e8fe7fce45d87330254131c4c
Sarath S Menon <sarathmenon.downey@gmail.com>
2019-08-20 17:39:59 +0530

    Update gym_mobilerobot.py

--- gym_mobilerobot/envs/gym_mobilerobot.py
+++ gym_mobilerobot/envs/gym_mobilerobot.py
@@ -96,7 +96,7 @@ class MobileRobotGymEnv(gym.Env):
     def getState(self, scan, type='normalized'):
         scan_range = []
         heading = self.heading
-        min_range = 0.10
+        min_range = 0.20
         done = False
 
         for i in range(len(scan.ranges)):
